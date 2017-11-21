8952a728bbda6f40ef6889fdc5bf740be739a8ee
Fengxx545 <569452415@qq.com>
2017-11-21 10:25:55 +0800

    测试
    这是一个测试工程

---
+++ NewEyeNurse/app/src/main/java/com/gzkj/eye/neweyenurse/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gzkj.eye.neweyenurse;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
